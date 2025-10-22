700197d4e36744260731d3ec26ba4ecd86fac2c3
Olga <olga.chitembo@metropolia.fi>
2025-10-22 15:17:27 +0300

    Clean up removed files and update project structure

--- web-dev/week1/gr-activities/web-dev/week1/gr-activities/lab2/main.js
+++
@@ -1,6 +0,0 @@
-const os = require('os');
-
-console.log("Platform: " + os.platform());
-console.log("Hostname: " + os.hostname());
-console.log("CPU cores: " + os.cpus());
-console.log("Architecture: " + os.arch());
